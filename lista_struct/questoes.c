dd18ee3e5dd5339b41de697a0ac2296ff0040578
antoniooseas <antonio.oseas@pixforce.ai>
2024-10-24 10:34:37 -0300

    feat: question 2

--- lista_struct/questoes.c
+++ lista_struct/questoes.c
@@ -13,10 +13,43 @@ void print_student(Student *student) {
     printf("Name: %s\n", student->name);
 }
 
+//questao 2
+typedef struct book {
+    char title[100];
+    float price;
+} Book;
+
+Book *create_books() {
+    Book *books = (Book *) malloc(5 * sizeof(Book));
+    return books;
+}
+void preencher_books(Book *books) {
+    for (int i = 0; i < 5; i++) {
+        printf("Title: ");
+        scanf("%s", books[i].title);
+        printf("Price: ");
+        scanf("%f", &books[i].price);
+    }
+}
+void print_books(Book *books) {
+    for (int i = 0; i < 5; i++) {
+        printf("Title: %s\n", books[i].title);
+        printf("Price: %.2f\n", books[i].price);
+    }
+}
+
 int main(){
-    Student student;
-    student.id = 1;
-    scanf("%s", student.name);
-    print_student(&student);
+    // questao 1
+    // Student student;
+    // student.id = 1;
+    // scanf("%s", student.name);
+    // print_student(&student);
+
+    //questao 2
+    Book *books;
+    books = create_books();
+    preencher_books(books);
+    print_books(books);
+    free(books);
     return 0;
 }
